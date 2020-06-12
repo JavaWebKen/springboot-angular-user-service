3ff562011ec8aef8b9527b25ae9b6991cd72e779
Ken Christie <javawebken@gmail.com>
2020-06-12 15:37:10 -0600

    Upated README and changed project name

---
+++ src/test/java/ken/SpringContextTest.java
@@ -0,0 +1,14 @@
+package ken;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = EcommerceApplication.class)
+public class SpringContextTest {
+
+	@Test
+	public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+	}
+}
